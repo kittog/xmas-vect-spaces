46962fe5e950849b0131f8eccaf87d13cf43864e
NataTCHA <114677507+NataTCHA@users.noreply.github.com>
2024-01-07 16:47:34 +0100

    Update maker_clean_corpus.py

--- scripts/maker_clean_corpus.py
+++ scripts/maker_clean_corpus.py
@@ -1,6 +1,8 @@
 import os
 import spacy
-
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+import numpy as np
+import pandas as pd
 def lire_contenu_corpus(path):
     contenu_corpus = []
     for fichier in os.listdir(path):
@@ -10,21 +12,22 @@ def lire_contenu_corpus(path):
                 contenu = file.read()
                 contenu_corpus.append(contenu)
     return contenu_corpus
-    
+
+import re
+
 def pretraiter_donnees(texte, stop_words_spacy):
     nlp = spacy.load('en_core_web_sm')
-    doc = nlp(texte)
-    tokens = [token.lemma_ for token in doc if token.text.lower() not in stop_words_spacy and not token.is_punct]
-    cleaned_tokens = [re.sub('\s+', ' ', token).strip() for token in tokens]  # Supprimer les espaces en trop
+    doc = nlp(texte)   
+    tokens = [token.lemma_.lower() for token in doc if token.text.isalpha() and token.text.lower() not in stop_words_spacy]
+    cleaned_tokens = [re.sub('\s+', ' ', token).strip() for token in tokens]
     return cleaned_tokens
-
 def main():
-    path_corpus = 'bla'
-    stop_words_spacy = spacy.lang.en.stop_words.STOP_WORDS
+    path_corpus = 'corpus'    
+    nlp = spacy.load('en_core_web_sm')
+    stop_words_spacy = nlp.Defaults.stop_words
     contenu_corpus = lire_contenu_corpus(path_corpus)
     documents_pretraites = [pretraiter_donnees(doc, stop_words_spacy) for doc in contenu_corpus]
-    for i, doc in enumerate(documents_pretraites):
-        print(f"Document {i + 1} après prétraitement:\n{doc}\n")
-
+    #for i, doc in enumerate(documents_pretraites):
+        #print(f"Document {i + 1} après prétraitement:\n{doc}\n")
 if __name__ == "__main__":
     main()
