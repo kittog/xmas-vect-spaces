c8f7e5497022f0bb7ee7d64ce71f963f4797719f
NataTCHA <114677507+NataTCHA@users.noreply.github.com>
2024-01-07 03:50:36 +0100

    Update maker_vector.py

--- scripts/maker_vector.py
+++ scripts/maker_vector.py
@@ -3,6 +3,7 @@ import spacy
 import gensim
 from sklearn.feature_extraction.text import CountVectorizer
 from maker_corpus_clean import lire_contenu_corpus, pretraiter_donnees
+
 def vectoriser_word2vec(documents_pretraites):
     model = gensim.models.Word2Vec(sentences=[doc.split() for doc in documents_pretraites], vector_size=100, window=5, min_count=1, workers=4)
     
@@ -26,23 +27,30 @@ def vectoriser_countvectorizer_reduit(documents_pretraites, dimensions_reduites=
     vectors_reduits = svd.fit_transform(vectors)
 
     return vectors_reduits
+
 def sauvegarder_vectors(vectors, feature_names, nom_fichier):
     with open(nom_fichier, 'w', encoding='utf-8') as file:
+        saved_vectors = set()  
         for word, vector in zip(feature_names, vectors):
-            vector_w = ' '.join(map(str, vector))
-            file.write(f"{word} {vector_w}\n")
+            key = f"{word}_{tuple(vector)}"
+            if key not in saved_vectors:
+                vector_w = ' '.join(map(str, vector))
+                file.write(f"{word} {vector_w}\n")
+                saved_vectors.add(key)
 
 def main():
-    path_corpus = 'bla'
+    path_corpus = 'corpus'
     stop_words_spacy = spacy.lang.en.stop_words.STOP_WORDS
     contenu_corpus = lire_contenu_corpus(path_corpus)
     documents_pretraites = [pretraiter_donnees(doc, stop_words_spacy) for doc in contenu_corpus]
+    
     vectors_word2vec = vectoriser_word2vec(documents_pretraites)
-    feature_names_word2vec = [word for doc in documents_pretraites for word in doc.split()]
+    feature_names_word2vec = [word for doc in documents_pretraites for word in doc]
     sauvegarder_vectors(vectors_word2vec, feature_names_word2vec, 'vectors_word2vec.txt')
-    vectors_countvectorizer = vectoriser_countvectorizer(documents_pretraites)
-    feature_names_countvectorizer = vectorizer.get_feature_names_out()
-    sauvegarder_vectors(vectors_countvectorizer, feature_names_countvectorizer, 'vectors_countvectorizer.txt')
+    
+    # vectors_countvectorizer = vectoriser_countvectorizer(documents_pretraites)
+    # feature_names_countvectorizer = ["word_" + str(i) for i in range(vectors_countvectorizer.shape[1])]
+    # sauvegarder_vectors(vectors_countvectorizer, feature_names_countvectorizer, 'vectors_countvectorizer.txt')
 
 if __name__ == "__main__":
     main()
