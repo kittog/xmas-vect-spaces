c093f13482f60b2bf8880daba5b5f5036b3237c3
NataTCHA <nminiconi@hotmail.fr>
2024-01-10 16:40:05 +0100

    voisin

--- scripts/tell_me_ur_neighbor.py
+++ scripts/tell_me_ur_neighbor.py
@@ -16,7 +16,7 @@ def read_vectors_file(file_path):
             vectors[word] = vector
     return vectors
 
-def find_knn_from_file(query_word, vectors, feature_names, k=5):
+def find_knn_from_file(query_word, vectors, feature_names, k=15):
     if query_word not in vectors:
         print(f"Le mot '{query_word}' n'est pas présent dans le fichier.")
         return
@@ -34,6 +34,12 @@ def find_knn_from_file(query_word, vectors, feature_names, k=5):
     print(f"Les {k} mots les plus similaires à '{query_word}':")
     for i, index in enumerate(filtered_indices[:k]):
         print(f"{i + 1}. {feature_names[index]} (distance: {filtered_distances[i]})")
+words_to_find = [
+    "christmas", "present", "light", "snow", "snowflake", "snowball", "bells", "song", "santa","claus",
+    "north", "sleigh", "elf", "reindeer", "angel", "chimney", "ornament", "xmas", "turkey", "fireplace",
+    "decoration", "bear", "cracker", "joy", "family", "mistletoe", "star", "tinsel", "festive", "candle",
+    "beard", "santa", "dinner", "socks", "to wrap up", "christian", "mass", "pudding", "stocking", "holly", "candy", "cold", "gift"
+]
 
 
 file_path = 'vectors_word2vec.txt'
@@ -42,3 +48,27 @@ feature_names_word2vec = list(vectors_word2vec.keys())
 
 query_word = "fox"
 find_knn_from_file(query_word, vectors_word2vec, feature_names_word2vec)
+def write_neighbors_to_file(words_to_find, vectors, feature_names, output_file='neighbors_output.txt', k=5):
+    with open(output_file, 'w', encoding='utf-8') as output:
+        for query_word in words_to_find:
+            if query_word not in vectors:
+                output.write(f"Le mot '{query_word}' n'est pas présent dans le fichier.\n")
+            else:
+                query_vector = vectors[query_word]
+                distances, indices = find_knn(query_vector, list(vectors.values()), k + 1)
+                filtered_distances = []
+                filtered_indices = []
+
+                for dist, ind in zip(distances[0], indices[0]):
+                    if feature_names[ind] != query_word:
+                        filtered_distances.append(dist)
+                        filtered_indices.append(ind)
+
+                output.write(f"Les {k} mots les plus similaires à '{query_word}':\n")
+                for i, index in enumerate(filtered_indices[:k]):
+                    output.write(f"{i + 1}. {feature_names[index]} (distance: {filtered_distances[i]})\n")
+                output.write("\n")
+
+# Utilisation de la fonction
+write_neighbors_to_file(words_to_find, vectors_word2vec, feature_names_word2vec)
+
